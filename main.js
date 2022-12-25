3b845f786fddb1ee8a02e07d864c661f73896231
camelPhonso <114600712+camelPhonso@users.noreply.github.com>
2022-12-25 09:43:03 +0000

    re-write generateAnalogous() as a recursion

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-let randomColor;
 const hexDigits = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 const reversedHexDigits = ['F','E','D','C','B','A',9,8,7,6,5,4,3,2,1,0];
 
+const analogousColors = document.querySelectorAll('.analogous--color');
+const complementaryColors = document.querySelectorAll('.complementary--color');
+
 //this function will generate a random color to attribute to the first .color-pair__top
 function generateColor(){
-    let temporaryHEX = '';
+    let temporaryHex = '';
     for (let i = 0; i < 6; i++){
         let digit = Math.floor((Math.random() * 16));
-        temporaryHEX += (hexDigits[digit]);
+        temporaryHex += (hexDigits[digit]);
     };
 
-    randomColor = temporaryHEX; //randomColor is a global variable in relation to which all other colors will be defined
+    window.randomColor = temporaryHex; //randomColor is a global variable in relation to which all other colors will be defined
 
-    window.firstColorDisplay = document.querySelector('.color-pair__top');
-    firstColorDisplay.setAttribute('name',temporaryHEX);
-    firstColorDisplay.style.backgroundColor = `#${randomColor}`;
-    firstColorDisplay.classList.add('has-color');
-
-    generateAnalogous(firstColorDisplay); //while testing - **REMOVE**
+    // window.firstColorDisplay = document.querySelector('.color-pair__top');
+    // firstColorDisplay.setAttribute('name',temporaryHex);
+    // firstColorDisplay.style.backgroundColor = `#${randomColor}`;
 
 };
 
-function generateAnalogous(div){
-    //this function will generate colors that are side by side with the argument
+function generateAnalogous(array, div){
+     //this function will generate colors that are side by side with the argument
     //this will be used to generate .color-pair__top colors following the first randomColor
+    if(div == analogousColors.length) return;
+    console.log('cycle - ' + div); //to test recursion
     
-    let temporaryArray = div.getAttribute('name').split();
-    let temporaryHEX = '';
-
-    console.log(temporaryArray); //testlog **REMOVE**
-
-    for (let i = 0; i < temporaryArray.length; i + 2){
-        new RegExp(/[A-F]/g).test(temporaryArray[i]) || new RegExp(/[6-9]/g).test(temporaryArray[i]) ? temporaryArray[i] = 6 || 'D' : null;
+    let baseColor = randomColor.split('');
+    let tempColor = [];
+    
+    let isHexNumber = (/[0-8]/);
+    let isHexLetter = (/[A-F]/);
+    
+    for (let i = 0; i < baseColor.length; i++){
+        isHexNumber.test(baseColor[i]) ? tempColor.push(Number(baseColor[i]) + 1) : isHexLetter.test(baseColor[i]) ? tempColor.push(reversedHexDigits[Math.floor(Math.random() * 7)]) : tempColor.push('1');
     };
-    console.log(temporaryArray);
 
+    console.log(`base color - ${baseColor}`);
+    console.log(`new color - ${tempColor}`);
+    randomColor = tempColor.join('');
+
+    array[div].style.backgroundColor = `#${randomColor}`
+    
+    generateAnalogous(array, div + 1);
 
 };
 
@@ -43,31 +51,19 @@ function generateComplementary(div){
     //this will be used to generate every .color-pair__bottom based on its pairing top
     
     let temporaryArray = div.getAttribute('name').split('');
-    let temporaryHEX = '';
+    let temporaryHex = '';
     
     for (let i = 0; i < temporaryArray.length; i++){
         for(let j = 0; j < hexDigits.length; j++){
-            temporaryArray[i] == hexDigits[j] ? temporaryHEX += reversedHexDigits[j] : null;
+            temporaryArray[i] == hexDigits[j] ? temporaryHex += reversedHexDigits[j] : null;
         };
     };
     
     // let parentColorPair = ; --> this line has been changed and not tested **CHECK && REMOVE**
-    let colorPairBottom = div.parentNode.lastElementChild;
-    
-    colorPairBottom.style.backgroundColor = `#${temporaryHEX}`;
-    colorPairBottom.classList.add('has-color');
-};
-
-
-
-function generateScheme(){
-    
-    let colorsDisplayed = document.querySelectorAll('.has-color');
-    for (let color of colorsDisplayed){
-        generateComplementary(color);
-    };
     
 };
+
+
 
 generateColor();
-generateScheme()
+generateAnalogous(analogousColors, 0);
